210ac3683a3104947009e63aa152e34ecabe8c51
Heisnberg22 <142800697+Heisnberg22@users.noreply.github.com>
2023-09-02 19:16:21 +0300

    done

---
+++ flutterapplicationday4/README.md
@@ -0,0 +1,3 @@
+# flutterapplicationday4
+
+A new Flutter project.
---
+++ flutterapplicationday4/android/app/src/main/kotlin/com/example/flutterapplicationday4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterapplicationday4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
